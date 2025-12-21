169b59ce23883794d04e65eb4df6fc91de393731
Ashwaq Alshahrani <93036626+Ashwaq-Alshahrani@users.noreply.github.com>
2025-12-21 18:39:50 +0300

    Initial commit

---
+++ src/bootcamp_data/config.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from dataclasses import dataclass
+
+@dataclass (frozen=True)
+class Paths:
+    root:Path
+    raw:Path
+    cache:Path
+    processed:Path
+    external:Path
+
+def make_paths(root:Path):
+    data=root/"data"
+    return Paths(
+        root=root,
+        raw=data/"row",
+        cache=data/"cache",
+        processed=data/"processed",
+        external=data/"external",
+    )
